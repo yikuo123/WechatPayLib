7958348604a6f8a8ea14347c8555951030834b80
yangheng_work <yangheng_work@live.com>
2016-09-14 18:04:08 +0800

    调整微信支付sdk的配置方式

--- lib/WxPay.Config.php
+++ lib/WxPay.Config.php
@@ -1,13 +1,8 @@
 <?php
 /**
-* 	配置账号信息
- *
- *使用方法：
- *定义类 WxPayConfig 并继承自 __WxPayConfig ,然后覆盖原有配置项
- *
+* 	配置账号信息*
 */
-
-class __WxPayConfig
+class WxPayConfig
 {
 	//=======【基本信息设置】=====================================
 	//
@@ -26,10 +21,10 @@ class __WxPayConfig
 	 * 获取地址：https://mp.weixin.qq.com/advanced/advanced?action=dev&t=advanced/dev&token=2005451881&lang=zh_CN
 	 * @var string
 	 */
-	const APPID = 'wx426b3015555a46be';
-	const MCHID = '1900009851';
-	const KEY = '8934e7d15453e97507ef794cf7b0519d';
-	const APPSECRET = '7813490da6f1265e4901ffb80afaa36f';
+	public static $APPID = 'wx426b3015555a46be';
+        public static $MCHID = '1900009851';
+        public static $KEY = '8934e7d15453e97507ef794cf7b0519d';
+        public static $APPSECRET = '7813490da6f1265e4901ffb80afaa36f';
 
 	//=======【证书路径设置】=====================================
 	/**
@@ -38,8 +33,8 @@ class __WxPayConfig
 	 * API证书下载地址：https://pay.weixin.qq.com/index.php/account/api_cert，下载之前需要安装商户操作证书）
 	 * @var path
 	 */
-	const SSLCERT_PATH = '../cert/apiclient_cert.pem';
-	const SSLKEY_PATH = '../cert/apiclient_key.pem';
+        public static $SSLCERT_PATH = '../cert/apiclient_cert.pem';
+        public static $SSLKEY_PATH = '../cert/apiclient_key.pem';
 
 	//=======【curl代理设置】===================================
 	/**
@@ -48,8 +43,8 @@ class __WxPayConfig
 	 * 默认CURL_PROXY_HOST=0.0.0.0和CURL_PROXY_PORT=0，此时不开启代理（如有需要才设置）
 	 * @var unknown_type
 	 */
-	const CURL_PROXY_HOST = "0.0.0.0";//"10.152.18.220";
-	const CURL_PROXY_PORT = 0;//8080;
+        public static $CURL_PROXY_HOST = "0.0.0.0";//"10.152.18.220";
+        public static $CURL_PROXY_PORT = 0;//8080;
 
 	//=======【上报信息配置】===================================
 	/**
@@ -59,6 +54,6 @@ class __WxPayConfig
 	 * 上报等级，0.关闭上报; 1.仅错误出错上报; 2.全量上报
 	 * @var int
 	 */
-	const REPORT_LEVENL = 1;
-	const NOTIFY_URL = '';
+        public static $REPORT_LEVENL = 1;
+        public static $NOTIFY_URL = '';
 }
